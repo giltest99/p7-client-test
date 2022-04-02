234513cb2edc763a2fbee1188615ce39f3167aac
Gilles <sellig.blater@free.fr>
2022-04-02 15:59:03 +0200

    Add main folders, add GlobalStyle, add W3.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import GlobalStyle from './styles/GlobaStyle';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
+
+      <GlobalStyle />
+
       <h1 className='firstLaunch'>Hello App !</h1>
+      
     </div>
   );
 }
